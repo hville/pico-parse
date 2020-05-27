6977316ba9c268e769c9841d7bee6d3897323120
hville <hugov@runbox.com>
2020-05-27 14:25:41 -0400

    fold, each

--- src/_tree.js
+++ src/_tree.js
@@ -15,3 +15,30 @@ Tree.prototype.add = function(itm) {
 Tree.prototype.toString = function() {
 	return this.cuts.length ? this.cuts.join('') : this.text.slice(this.i, this.j)
 }
+Tree.prototype.each = function(ante, post) {
+	//loop each children with a transformation on the way down and on the way up
+	var set = this.set,
+			i = 0
+	while(i<set.length) {
+		this.err -= set[i].err
+		if (ante && set[i].rule[ante]) set[i] = set[i].rule[ante](set[i], i)
+		if (set[i]) {
+			set[i].each(ante, post)
+			if (post && set[i].rule[post]) set[i] = set[i].rule[post](set[i], i)
+		}
+		if (set[i]) this.err += set[i++].err
+		else set.splice(i,1)
+	}
+	return this
+}
+Tree.prototype.fold = function(ante, post, crop) {
+	//loop each children with a transformation on the way down and on the way up
+	var set = this.set,
+			i = 0
+	while(i<set.length) {
+		if (ante && set[i].rule[ante]) crop = set[i].rule[ante](crop, set[i], i)
+		set[i].fold(ante, post, crop)
+		if (post && set[i].rule[post]) crop = set[i].rule[post](crop, set[i], i)
+	}
+	return crop
+}
