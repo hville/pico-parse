c9a937c74465ca0f49ffab8795c77efd287b6bfd
hville <hugov@runbox.com>
2023-09-03 06:04:55 -0400

    peg and tests

--- tst/pegparser.js
+++ tst/pegparser.js
@@ -1,5 +1,4 @@
-import $ from '../parsers.js'
-
+export default $ => {
 //# helpers
 const identifier = /[\p{ID_Start}\$_][\p{ID_Continue}\$_\u200C\u200D]*/u
 
@@ -33,13 +32,14 @@ itm = $`|`(SEQ, pre),
 ANY = $.any(itm, $`+`(_, '|', _, itm) ), //e1 / e2 binary 1 Prioritized Choice //Expression = Sequence (SLASH Sequence)*
 DEF = $.def(PID, _, '=', _, exp ),//Definition = Identifier LEFTARROW Expression
 // Error Management
-ERR = $.err( /[^\s]*/ ),
+ERR = $.err( /[^\s]+/ ),
 // final grammar
 PEG = $.peg( _, $`|`(DEF, exp, ERR), $`*`( _, $`|`(DEF, ERR) ), _ )
 
 exp.set(ANY, SEQ, AND, NOT, GET, FEW, OPT, RUN, prm)
 
 const m = new Map // memory to store PEG references before being declared
+
 const acts = {
 	lit: (r,s) => s,
 	dot: () => /[^]/,
@@ -49,10 +49,20 @@ const acts = {
 	ops: r => typeof r[0] === 'function' ? r.shift()(...r) : r.pop()(...r),
 	seq: r => $(...r),
 	any: r => $`|`(...r),
-	def: r => m.has(r[0]) ? Object.assign( m.get(r[0]), $(r[1]) ) : m.set(r[0], $(r[1])).get(r[0]), // do not change refs,
+	def: ([k,v]) => { // do not change refs,
+		if (!v.peek) v = $(v)
+		v.id = k
+		if (m.has(k)) {
+			v = Object.assign( m.get(k), v )
+			m.delete(k)
+		}
+		m.set(k, v)
+		return v
+	},
 	ref: r => m.get(r[0]) ?? m.set(r[0], $()).get(r[0]), // create if it does not exist
 	err: r =>(r.error = `error at ${ r.i }.`, r),
 	peg: () => m.values().next()[(m.clear(), 'value')]
 }
 
-export default (source, actions=acts) => PEG.scan(source, actions)
+return (source, actions=acts) => PEG.scan(source, actions)
+}
